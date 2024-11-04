4469f7871444f6d69d59179dc4e2fcb5ffe0c535
Isara Nakavisute <isara_nakavisute@hotmail.com>
2024-11-04 16:45:52 +0700

    change text color

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,13 +11,21 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-                      title: Text("This is a flutter test"),
-                      backgroundColor: Colors.redAccent,
-                    ),
-                  body: Center(
-                               child: Text('Nov 4 2024'),
-                              ),
-                   );
+        title: Center(child: Text("This is a flutter test")),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Center(
+        child: Text(
+          'Nov 4 2024',
+          style: TextStyle(
+            color: Colors.white, // Set the text color to blue
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
